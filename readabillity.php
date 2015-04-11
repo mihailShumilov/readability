61605d1a49740b22bb9cbbb343680623a4457cec
Mihail Shumilov <mschumilow@gmail.com>
2015-04-11 19:55:30 +0300

    first good result

--- readabillity.php
+++ readabillity.php
@@ -14,8 +14,11 @@
         private $url;
         private $data;
         private $dom;
+        private $maxScore = 0;
+        private $contentNode = false;
 
-        private $badTags = ['header', 'footer', 'nav', 'script'];
+        private $badTags = [ 'header', 'footer', 'nav', 'script', 'sidebar', 'noscript', 'noindex', 'table', 'ul' ];
+
 
         public function __construct( $url )
         {
@@ -30,14 +33,19 @@
         {
             if ($this->data = PageLoader::load( $this->url )) {
                 $this->createDomObject();
-                return $this->clean();
+                $this->clean();
+                $this->calculateWeight();
 
-            }else{
+                $Document = new \DOMDocument();
+                $Document->appendChild( $Document->importNode( $this->contentNode, true ) );
+                return $Document->saveHTML();
+            } else {
                 return false;
             }
         }
 
-        protected function createDomObject(){
+        protected function createDomObject()
+        {
             $this->data = mb_convert_encoding( $this->data, 'HTML-ENTITIES', "UTF-8" );
             $this->dom  = new \DOMDocument( "1.0", "utf-8" );
             libxml_use_internal_errors( true );
@@ -45,8 +53,9 @@
             $this->dom->loadHTML( $this->data );
         }
 
-        private function clean(){
-            foreach($this->badTags as $tag) {
+        private function clean()
+        {
+            foreach ($this->badTags as $tag) {
                 $elements = $this->dom->getElementsByTagName( $tag );
                 while ($el = $elements->item( 0 )) {
                     $el->parentNode->removeChild( $el );
@@ -55,4 +64,39 @@
             return $this->dom->saveHTML();
         }
 
+        private function calculateWeight()
+        {
+            $body  = $this->dom->getElementsByTagName( 'body' )->item( 0 );
+            $level = 0;
+            $this->processNode( $body, $level );
+            return $this->dom->saveHTML();
+        }
+
+        /**
+         * @param DOMElement $node
+         * @param int $level
+         */
+        private function processNode( $node, $level )
+        {
+            $level ++;
+
+            $text = $node->nodeValue;
+            preg_replace( "/\s+/", "", $text );
+            $textLength = strlen( $text );
+            $score      = $textLength * $level;
+
+            if ($this->maxScore < $score) {
+                $this->maxScore    = $score;
+                $this->contentNode = $node;
+            }
+
+            $node->setAttribute( "level", $level );
+            $node->setAttribute( "charcount", $textLength );
+            $node->setAttribute( "score", $score );
+            foreach ($node->childNodes as $element) {
+                if ($element->childNodes) {
+                    $this->processNode( $element, $level );
+                }
+            }
+        }
     }
