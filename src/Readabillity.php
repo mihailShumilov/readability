e56f55d0f5abf3f174e13901abdcf4ede2a39291
mihail shumilov <mshumilow@gmail.com>
2019-01-22 07:33:06 +0200

    improove readability

--- src/Readabillity.php
+++ src/Readabillity.php
@@ -19,6 +19,7 @@ class Readabillity {
     private $charset;
     private $maxPolsition = 0;
     private $charCount = [];
+    private $maxLevel = 0;
 
     private $badTags = [
         'header',
@@ -30,7 +31,6 @@ class Readabillity {
         'noindex',
         //            'table',
         //            'ul',
-        //            'form',
         'input',
         'button',
         'ol',
@@ -40,7 +40,8 @@ class Readabillity {
         'dd',
         'dt',
         //            'li',
-        'time'
+        'time',
+        'form'
     ];
 
     private $baseBadCssSelector = [
@@ -50,12 +51,12 @@ class Readabillity {
         "//*[php:function('preg_match', '/coment/iu', string(@class))>0]",
         "//*[php:function('preg_match', '/footer/iu', string(@id))>0]",
         "//*[php:function('preg_match', '/footer/iu', string(@class))>0]",
-//        "//*[php:function('preg_match', '/sidebar/iu', string(@id))>0]",
-//        "//*[php:function('preg_match', '/sidebar/iu', string(@class))>0]",
-//        "//*[php:function('preg_match', '/header/iu', string(@class))>0]",
-//        "//*[php:function('preg_match', '/header/iu', string(@class))>0]",
-//        "//*[php:function('preg_match', '/menu/iu', string(@class))>0]",
-//        "//*[php:function('preg_match', '/menu/iu', string(@class))>0]",
+        //        "//*[php:function('preg_match', '/sidebar/iu', string(@id))>0]",
+        //        "//*[php:function('preg_match', '/sidebar/iu', string(@class))>0]",
+        //        "//*[php:function('preg_match', '/header/iu', string(@class))>0]",
+        //        "//*[php:function('preg_match', '/header/iu', string(@class))>0]",
+        //        "//*[php:function('preg_match', '/menu/iu', string(@class))>0]",
+        //        "//*[php:function('preg_match', '/menu/iu', string(@class))>0]",
         //            "//*[php:function('preg_match', '/sidebar/iu', string(@id))>0]",
         //            "//*[php:function('preg_match', '/sidebar/iu', string(@class))>0]"
 
@@ -71,6 +72,13 @@ class Readabillity {
         "//*[php:function('preg_match', '/error/iu', string(@id))>0]",
         "//*[php:function('preg_match', '/error/iu', string(@class))>0]"
     ];
+
+    private $contentSelectors = [
+        '//*[@itemprop=\'articleBody\']',
+        //        '//article',
+    ];
+
+    private $lastContent = false;
 
 
     public function __construct($url = false, $rawHtml = false) {
@@ -88,7 +96,7 @@ class Readabillity {
             $this->data = $this->loadAsUTF8($this->url);
         }
         if ($rawHtml) {
-            $this->data = $this->prepareRawData($rawHtml);
+            $this->data = $this->cleanupHtml($this->prepareRawData($rawHtml));
         }
 
     }
@@ -111,6 +119,7 @@ class Readabillity {
 
     public function getTitle() {
         $result = '';
+
         if ($this->charset) {
             $result = mb_convert_encoding($this->title, 'UTF-8', $this->charset);
         } else {
@@ -123,46 +132,53 @@ class Readabillity {
     public function getContent() {
         if ($this->data) {
 //                return $this->data;
+
             $this->createDomObject();
             $this->clean();
 
-            $this->calculateWeight(true);
+            if (!$this->hardCodedDetect()) {
 
-            $this->clearContentNode();
+                $this->calculateWeight(true);
+
+                $this->clearContentNode();
 
 //                $Document = new \DOMDocument();
 //                $Document->appendChild( $Document->importNode( $this->contentNode, true ) );
 //                $this->data = $Document->saveHTML();
 
 //                return $this->dom->saveHTML();
+
 //                return $this->data;
 
-            $this->data = $this->dom->saveHTML();
+                $this->data = $this->dom->saveHTML();
 
-            $this->createDomObject();
+                $this->createDomObject();
 
-            $this->calculateWeight();
-            $this->clearByScore();
+                $this->calculateWeight();
+                $this->clearByScore();
 
-            $this->data = $this->dom->saveHTML();
+                $this->data = $this->dom->saveHTML();
+
 //            return $this->data;
 
-            $this->createDomObject();
-            $this->calculateWeight();
-            $this->clearContentNode();
-            $this->clearByScore();
-            $this->clearByPosition();
+                $this->createDomObject();
+                $this->calculateWeight();
+                $this->clearContentNode();
+                $this->clearByScore();
+                $this->clearByPosition();
 
 //            $this->cleanByCSS();
+            }
 
+            $this->detectTitle();
 
-            return $this->prepareResponseContent($this->dom->saveHTML());
+            return $this->cleanupHtml($this->dom->saveHTML());
         } else {
             return false;
         }
     }
 
-    protected function prepareResponseContent($data) {
+    protected function cleanupHtml(string $data): string {
         $tidy = tidy_parse_string($data, [
             'clean'            => true,
             'drop-empty-paras' => true,
@@ -176,17 +192,20 @@ class Readabillity {
         return $tidy->body()->value;
     }
 
-    protected function createDomObject() {
+    protected function createDomObject(): void {
         $this->data = mb_convert_encoding($this->data, 'UTF-8', 'UTF-8');
         $this->dom  = new \DOMDocument('1.1', 'UTF-8');
         libxml_use_internal_errors(true);
         $this->dom->preserveWhiteSpace = false;
         $this->dom->loadHTML('<?xml encoding="utf-8" ?>' . $this->data);
-        $this->title =  $this->dom->getElementsByTagName("title")->item(0)->textContent;
-
+        if(!$this->title) {
+            if ($titleNode = $this->dom->getElementsByTagName("title")->item(0)) {
+                $this->title = $titleNode->textContent;
+            }
+        }
     }
 
-    private function clean() {
+    private function clean(): string {
         $xpath = new \DOMXpath($this->dom);
 
         foreach ($this->badTags as $tag) {
@@ -209,7 +228,7 @@ class Readabillity {
         return $this->dom->saveHTML();
     }
 
-    private function cleanByCSS() {
+    private function cleanByCSS(): string {
         $xpath = new \DOMXpath($this->dom);
         $xpath->registerNamespace('php', 'http://php.net/xpath');
         $xpath->registerPHPFunctions();
@@ -224,10 +243,10 @@ class Readabillity {
         return $this->dom->saveHTML();
     }
 
-    private function calculateWeight($storeCharCount = false) {
+    private function calculateWeight($storeCharCount = false): string {
         $body  = $this->dom->getElementsByTagName('body')->item(0);
         $level = 0;
-        $this->processNode($body, $level,$storeCharCount);
+        $this->processNode($body, $level, $storeCharCount);
         return $this->dom->saveHTML();
     }
 
@@ -235,107 +254,152 @@ class Readabillity {
      * @param DOMElement $node
      * @param int        $level
      */
-    private function processNode($node, $level, $storeCharCount = false, &$position = 1) {
-        $level++;
-        $selfPosition = $position;
-
-        $text = $node->nodeValue;
-        preg_replace('/\s+/', '', $text);
-        $textLength = strlen($text);
-//            $score      = ( $textLength * $level ) / $position;
-        $score = ($textLength * $level);
-
-        if ($this->maxScore < $score) {
-            $this->maxScore    = $score;
-            $this->contentNode = $node;
-        }
-
-        if ($this->maxPolsition < $selfPosition) {
-            $this->maxPolsition = $selfPosition;
-        }
-
+    private function processNode($node, $level, $storeCharCount = false, &$position = 1): int {
         $linkCount = 1;
-
-
-        foreach ($node->childNodes as $element) {
-            $position++;
-            if ($element->childNodes) {
-                $linkCount += $this->processNode($element, $level, $storeCharCount, $position);
+        if (is_object($node)) {
+            $level++;
+            if($this->maxLevel < $level){
+                $this->maxLevel = $level;
             }
-        }
+            $selfPosition = $position;
 
-        $ls        = $textLength / $linkCount;
-        $linkScore = 'good';
-        if ($ls <= 10 && $ls > 0) {
-            $linkScore = 'bad';
-        }
-        if ('a' == $node->tagName) {
-            if ($textLength <= 40) {
-                $linkScore = 'good';
-            } else {
-                $linkScore = 'bad';
+            $text = $node->nodeValue;
+            preg_replace('/\s+/', '', $text);
+            $textLength = strlen($text);
+//            $score      = ( $textLength * $level ) / $position;
+            $score = ($textLength * $level);
+
+            if ($this->maxScore < $score) {
+                $this->maxScore    = $score;
+                $this->contentNode = $node;
             }
-        }
 
-        $removeByPosition = 'no';
-        $positionScore    = $selfPosition / $this->maxPolsition;
-        if ($positionScore > 0.38) {
-            $removeByPosition = 'yes';
-        }
+            if ($this->maxPolsition < $selfPosition) {
+                $this->maxPolsition = $selfPosition;
+            }
 
-        if($storeCharCount){
-            $this->charCount[] = $textLength;
-        }else{
-            if($this->charCount) {
-                $avg = array_sum($this->charCount) / count($this->charCount);
-                $node->setAttribute('avgcharcount', $avg);
-                if($textLength > $avg){
-                    $removeByPosition = 'no';
+
+            foreach ($node->childNodes as $element) {
+                $position++;
+                if ($element->childNodes) {
+                    $linkCount += $this->processNode($element, $level, $storeCharCount, $position);
                 }
             }
+
+            $ls        = $textLength / $linkCount;
+            $linkScore = 'good';
+            if ($ls <= 10 && $ls > 0) {
+                $linkScore = 'bad';
+            }
+            if ('a' == $node->tagName) {
+                if ($textLength <= 40) {
+                    $linkScore = 'good';
+                } else {
+                    $linkScore = 'bad';
+                }
+            }
+
+            $removeByPosition = 'no';
+            $positionScore    = $selfPosition / $this->maxPolsition;
+            if ($positionScore > 0.38) {
+                $removeByPosition = 'yes';
+            }
+
+            if ($storeCharCount) {
+                $this->charCount[] = $textLength;
+            } else {
+                if ($this->charCount) {
+                    $avg = array_sum($this->charCount) / count($this->charCount);
+                    $node->setAttribute('avgcharcount', (int)$avg);
+                    if (($textLength / $avg) > 2) {
+                        $removeByPosition = 'no';
+                    } else {
+//                        $removeByPosition = 'yes';
+                    }
+                }
+            }
+
+//            if(($level / $this->maxLevel) > 0.68){
+//                $removeByPosition = 'yes';
+//            }
+
+            $node->setAttribute('level', $level);
+            $node->setAttribute('maxLevel', $this->maxLevel);
+            $node->setAttribute('charcount', $textLength);
+            $node->setAttribute('score', $score);
+            $node->setAttribute('linkcount', $linkCount);
+            $node->setAttribute('linkscore', $linkScore);
+            $node->setAttribute('linkscorevalue', $ls);
+            $node->setAttribute('position', $selfPosition);
+            $node->setAttribute('positionScore', $positionScore);
+            $node->setAttribute('removeByPosition', $removeByPosition);
+
+            if ('a' == $node->tagName) {
+                $linkCount++;
+            }
         }
-
-        $node->setAttribute('level', $level);
-        $node->setAttribute('charcount', $textLength);
-        $node->setAttribute('score', $score);
-        $node->setAttribute('linkcount', $linkCount);
-        $node->setAttribute('linkscore', $linkScore);
-        $node->setAttribute('linkscorevalue', $ls);
-        $node->setAttribute('position', $selfPosition);
-        $node->setAttribute('positionScore', $positionScore);
-        $node->setAttribute('removeByPosition', $removeByPosition);
-
-        if ('a' == $node->tagName) {
-            $linkCount++;
-        }
-
         return $linkCount;
     }
 
-    private function clearContentNode() {
+    private function clearContentNode(): void {
         $this->tryDetectMainContentNode();
     }
 
-    private function tryDetectMainContentNode() {
-        foreach ($this->contentNode->childNodes as $element) {
-            if (is_a($element, 'DOMElement')) {
-                if ((($element->getAttribute('score') / $this->maxScore) > 0.4) && ('body' != $element->tagName)) {
-                    $this->contentNode = $element;
-                    $this->maxScore    = $element->getAttribute('score');
-                    break;
+    private function tryDetectMainContentNode(): void {
+        if ($this->contentNode && (!empty((array)$this->contentNode))) {
+//            echo "<h3>kokok</h3>";
+//            echo "<pre>";
+//            var_dump($this->contentNode);
+//            echo "</pre>";
+            $this->lastContent = $this->contentNode;
+            foreach ($this->contentNode->childNodes as $element) {
+                if (is_a($element, 'DOMElement')) {
+                    if ((($element->getAttribute('score') / $this->maxScore) > 0.4) && ('body' != $element->tagName)) {
+                        $this->contentNode = $element;
+                        $this->maxScore    = $element->getAttribute('score');
+                        break;
+                    }
                 }
             }
+        } else {
+//            echo "<h3>lopllp</h3>";
+//            echo "<pre>";
+//            var_dump($this->lastContent);
+//            echo "</pre>";
+            if ($this->lastContent) {
+                $this->contentNode = $this->lastContent;
+            }
         }
+
     }
 
     /**
      * @param bool|DOMElement $node
      */
-    private function clearByScore($node = false) {
+    private function clearByScore($node = false): string {
         $xpath = new \DOMXpath($this->dom);
 
         foreach ($xpath->query("//*[@linkscore='bad']") as $node) {
-            $node->parentNode->removeChild($node);
+            if (
+                (
+                    (
+                        ((int)$node->getAttribute('charcount') / (int)$node->getAttribute('avgcharcount')) < 3
+                    )
+                    ||
+                    (
+                        (int)$node->getAttribute('linkcount') > (int)$node->getAttribute('avgcharcount')
+                    )
+                )
+    //                &&
+    //                (
+    //                    ((int)$node->getAttribute('level') / (int)$node->getAttribute('maxlevel')) > 0.5
+    //                )
+                && (
+                    (int)$node->getAttribute('level') > 3
+                )
+            ) {
+                $node->parentNode->removeChild($node);
+            }
         }
 
         return $this->dom->saveHTML();
@@ -345,7 +409,7 @@ class Readabillity {
     /**
      * @param bool|DOMElement $node
      */
-    private function clearByPosition($node = false) {
+    private function clearByPosition($node = false): string {
         $xpath = new \DOMXpath($this->dom);
 
         foreach ($xpath->query("//*[@removeByPosition='yes']") as $node) {
@@ -394,18 +458,11 @@ class Readabillity {
                         $data = mb_convert_encoding($data, "UTF-8");
                     }
                 }
-                $tidyConfig = [
-                    'clean'            => true,
-                    'drop-empty-paras' => true,
-                    'fix-backslash'    => true,
-                    'fix-bad-comments' => true,
-                    'fix-uri'          => true,
-                    'hide-comments'    => true
-                ];
-                $tidy       = tidy_parse_string($data, $tidyConfig, 'utf8');
-                $tidy->cleanRepair();
-                $body = $tidy->html();
-                return $body->value;
+
+                $this->data = $data;
+                $this->createDomObject();
+
+                return $this->cleanupHtml($data);
             } catch (Exception $e) {
                 return false;
             }
@@ -413,6 +470,45 @@ class Readabillity {
             throw new Exception("No valid url: `{$url}`", 505);
         }
 
+    }
+
+
+    public function detectTitle(): void {
+        if ($titleNode = $this->dom->getElementsByTagName("h1")->item(0)) {
+            $titleNode->parentNode->removeChild($titleNode);
+            $this->title = $titleNode->textContent;
+        }
+    }
+
+    protected function hardCodedDetect(): bool {
+        $result = false;
+
+        $xpath = new \DOMXpath($this->dom);
+
+        $prevDataLength = 0;
+
+        foreach ($this->contentSelectors as $tag) {
+            if ($contentNode = $xpath->query($tag)) {
+                $data = [];
+                foreach ($contentNode as $node) {
+                    $data[] = $node->nodeValue;
+                }
+
+                $possibleData = implode("</p><p>", $data);
+
+                if (strlen($possibleData) > $prevDataLength) {
+                    $prevDataLength = strlen($possibleData);
+                    $this->data     = $possibleData;
+                    $this->data     = "<p>" . $this->data . "</p>";
+                    $result         = true;
+                }
+            }
+        }
+
+        if ($result) {
+            $this->createDomObject();
+        }
+        return $result;
     }
 
 }
